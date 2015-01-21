92b032e20329f37eb3108a67b2a328d58a079336
pchtsp <pchtsp@gmail.com>
2015-01-21 23:37:06 +0100

    cambios en ficheros relativos a commit anterior.

--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,11 @@ var mainState = {
 		this.game.load.spritesheet("asteroid", "img/asteroid1.png",72,72,19);
 		this.game.load.spritesheet("asteroid2", "img/asteroid2.png",72,72,19);
 		this.game.load.spritesheet("asteroid3", "img/asteroid3.png",72,72,19);
+		this.game.load.image("client1", "img/client1.jpeg");
+		this.game.load.image("client2", "img/client2.jpeg");
+		this.game.load.image("client3", "img/client3.jpeg");
 		this.load.bitmapFont('minecraftia','fonts/minecraftia/minecraftia.png','fonts/minecraftia/minecraftia.xml');
+		this.game.load.spritesheet("explosion", "img/explosion.png",100,100,74);
         //this.ROTATION_SPEED = 90;
     },
 
@@ -48,15 +52,26 @@ var mainState = {
 			//auxiliar
 			var _lifespan=this.game.rnd.integerInRange(10,30)
 			var x= this.game.rnd.integerInRange(50,this.game.world.width-100);
+			var y= this.game.rnd.integerInRange(50,this.game.world.height-100);			
+			//generation:
+			asteroid= new Asteroid(this.game,x,y);
+			asteroid.animations.add('turn');
+			asteroid.animations.play('turn', 2, true);
+			asteroids.add(asteroid);
+		}
+
+		clients= this.game.add.group();
+		for (var z = 0; z < 2; z++) {
+			//auxiliar
+			var _lifespan=this.game.rnd.integerInRange(10,30)
+			var x= this.game.rnd.integerInRange(50,this.game.world.width-100);
 			var y= this.game.rnd.integerInRange(50,this.game.world.height-100);
 			var score= this.game.rnd.integerInRange(50,100);
 			var type=this.game.rnd.integerInRange(2,3);
 			
 			//generation:
-			asteroid= new Asteroid(this.game,x,y, undefined, undefined, _lifespan, score, type);
-			asteroid.animations.add('turn');
-			asteroid.animations.play('turn', 2, true);
-			asteroids.add(asteroid);
+			client= new Client(this.game,x,y, undefined, undefined, _lifespan, score, type);
+			clients.add(client);
 		}
 		
 		//Agrego pizzas
@@ -70,8 +85,6 @@ var mainState = {
 			var type=this.game.rnd.integerInRange(2,3);
 			this.pizza= new Package(this.game,x,y,undefined, undefined, type);
 			pizzas.add(this.pizza);
-			//this.pizza.reset(x,y);
-			//this.pizza.revive();
 		};
 		
         //Agrego teclas
@@ -112,7 +125,8 @@ var mainState = {
 		this.game.physics.arcade.overlap(nave,pizzas, pickPackage,null,this);
 		
 		//colisiones con asteroides (grupo "asteroids")
-		this.game.physics.arcade.overlap(nave,asteroids, leavePackage,null,this);
+		this.game.physics.arcade.overlap(nave,asteroids, hitAsteroid,null,this);
+		this.game.physics.arcade.overlap(nave,clients, leavePackage,null,this);
 		
 		//si se ha capturado una pizza, esta debe aparecer junto con la nave:
 		if (this.packageCaptured && this.packageCapturedNumber>=0) {
@@ -139,7 +153,7 @@ var mainState = {
 		this.scoreText.text= 'Score: '+this.score;
 		
 		//para cada asteroide: dibujamos el contador al lado
-		asteroids.forEachAlive(drawLifespan, this);
+		clients.forEachAlive(drawLifespan, this);
     },
 	shutdown: function() {
 		asteroids.destroy(true);
@@ -156,39 +170,54 @@ function getRemainingTime(maxTime, startTime, _game) {
 function pickPackage(_ship,_package) {
 	if (_package.available) {
 		this.packageCapturedNumber= pizzas.getChildIndex(_package);
-		this.packageCaptured=true;
+	
+	true;
 	};
 	
 };
 
-function drawLifespan(_package) {
-	if (_package.lifespan>=0) _package.lifespanText.text=Math.floor(_package.lifespan/1000);
+function drawLifespan(_client) {
+	if (_client.lifespan>=0) _client.lifespanText.text=Math.floor(_client.lifespan/1000);
 	
 };
 
-function leavePackage(_ship,_asteroid) {
+function hitAsteroid(_ship,_asteroid) {
+	//si chocamos con un asteroide: morimos (evidentemente)
+	//ToDo: matar los contadores
+	explosion = this.game.add.sprite(_ship.body.x,_ship.body.y,"explosion");
+	explosion.anchor.setTo(0.5,0.5);
+	explosion.animations.add('explode');
+	explosion.animations.play("explode", 30, false, true);
+	nave.kill();
+	pizzas.destroy();
+	asteroids.destroy();
+	var scoreboard= new Scoreboard(this.game);
+	scoreboard.show(50);
+};
+
+function leavePackage(_ship,_client) {
 	if (this.packageCaptured && this.packageCapturedNumber>=0) {
 		_pizza= pizzas.getChildAt(this.packageCapturedNumber);
 		//debemos garantizar que solo podemos dejar la pizza 
 		//que el cliente ha pedido (es decir: que sea del mismo tipo)
 		//también: ese asteroide no puede tener pizza ya.
-		if (_pizza.type==_asteroid.type && _asteroid.packageDeliveredNum==-1) {
+		if (_pizza.type==_client.type && _client.packageDeliveredNum==-1) {
 			//al dejar la pizza:
 			//se aumenta el puntaje, 
-			this.score+=_asteroid.points;
+			this.score+=_client.points;
 			
 			//se apunta el paquete que fue entregado en el asteroide:
-			_asteroid.packageDeliveredNum=this.packageCapturedNumber;
+			_client.packageDeliveredNum=this.packageCapturedNumber;
 			
 			//se termina de contar el tiempo al destino:
-			_asteroid.lifespan=-1;
+			_client.lifespan=-1;
 						
 			//se reinicializan los marcadores que indican qué paquete se tiene en la nave
 			this.packageCaptured=false;
 			this.packageCapturedNumber=-1;
 			//se ubica el paquete alrededor del destino
-			_pizza.x=_asteroid.x+15;
-			_pizza.y=_asteroid.y-15;
+			_pizza.x=_client.x+15;
+			_pizza.y=_client.y-15;
 			
 			//se prohíbe recoger el paquete otra vez:
 			_pizza.available=false;
